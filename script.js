f5fb30ab0a76233f9aecf3fad172a7e675f99dcd
erikamattje <erikamattje@gmail.com>
2026-08-31 23:45:32 -0300

    feat: create Pixel Market

---
+++ script.js
@@ -0,0 +1,50 @@
+const container = document.querySelector("#produtos");
+const botoes = document.querySelectorAll(".categoria");
+let categoriaSelecionada = "Todos";
+
+fetch("produtos.json")
+    .then(resposta => resposta.json())
+    .then(dados => {
+
+        function mostrarProdutos() {
+            container.innerHTML = "";
+
+            let produtosFiltrados;
+
+            if (categoriaSelecionada === "Todos") {
+                produtosFiltrados = dados;
+            } else {
+                produtosFiltrados = dados.filter(produto => produto.categoria === categoriaSelecionada);
+            }
+
+            for (const produto of produtosFiltrados) {
+                const card = document.createElement("div");
+
+                card.classList.add("card");
+
+                card.innerHTML = `
+                    <img src="${produto.imagem}" alt="${produto.nome}">
+                    <h2>${produto.nome}</h2>
+                    <p class="preco">${produto.preco.toLocaleString("pt-BR", {
+                        style: "currency",
+                        currency: "BRL"
+                    })}</p>
+                    <div class="info">
+                        <p>🎮 ${produto.categoria}</p>
+                        <p>📦 ${produto.estoque} em estoque</p>
+                    </div>
+                `;
+                container.appendChild(card);
+            }
+        }
+
+        mostrarProdutos();
+
+        for (const botao of botoes) {
+            botao.addEventListener("click", () => {
+                categoriaSelecionada = botao.textContent;
+                mostrarProdutos();
+            });
+        }
+
+    });
